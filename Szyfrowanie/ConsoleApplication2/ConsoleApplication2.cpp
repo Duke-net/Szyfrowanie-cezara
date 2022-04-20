50e42c9fe47e9026a2682904738881b75a394a3e
unknown <Administrator@K-1928-07.studzpsb.local>
2022-04-20 10:49:58 +0200

    error

---
+++ Szyfrowanie/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void Funkcja2(int klucz, char wyraz[]);
+
+int main()
+{
+    int klucz;
+    char wyraz[200];
+    
+    cout << "Podaj klucz: ";
+    cin >> klucz;
+    cout << "Podaj wyraz: ";
+    cin >> wyraz;
+
+    Funkcja2(klucz, wyraz);
+    cout << wyraz;
+}
+
+void Funkcja2(int klucz, char wyraz[]) {
+    int dlugosc = strlen(wyraz);
+    if (!(klucz >= -26 && klucz <= 26)) {
+        return;
+    }
+    if (klucz >= 0) {
+        for (int i = 0; i < dlugosc; i++) {
+            if (wyraz[1] + klucz <= 'z') {
+                wyraz[1] += klucz;
+            }
+            else
+            {
+                wyraz[1] = wyraz[1] + klucz - 26;
+            }
+        }
+    }
+    else
+    {
+        for (int i = 0; i < dlugosc; i++) {
+            if (wyraz[1] + klucz >= 'a') {
+                wyraz[1] += klucz;
+            }
+            else
+            {
+                wyraz[1] = wyraz[1] + klucz + 26;
+            }
+        }
+    }
+}
